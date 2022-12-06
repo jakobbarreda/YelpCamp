c06835e7d361a8e12f41855e9ba134440d862a0d
jakobbarreda <74332138+jakobbarreda@users.noreply.github.com>
2022-12-06 17:54:17 -0600

    Add files via upload

---
+++ models/campground.js
@@ -0,0 +1,70 @@
+// shortning mongoose.Schema call and require
+const { string, number } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review");
+
+// ImageSchema
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
+
+const opts = { toJSON: { virtuals: true } };
+
+// creating campGroundSchema structure
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    images: [ImageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    price: Number,
+    description: String,
+    location: String,
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
+
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+  return `<strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+  <p>${this.description.substring(0, 30)}...}</p>`;
+});
+
+// when a campground gets deleted it triggers this function to delete
+// all reviews on the campground
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
+
+// allows uses in other files
+module.exports = mongoose.model("Campground", CampgroundSchema);
