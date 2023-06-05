2efe470fdc140a631851addba65c5aec7778bdb4
jakob barreda <jakobbarreda@gmail.com>
2023-06-04 20:05:38 -0500

    middleware change

--- middleware.js
+++ middleware.js
@@ -1,18 +1,18 @@
-const { campgroundSchema, reviewSchema } = require("./schemas.js");
-const ExpressError = require("./utils/ExpressError");
-const Campground = require("./models/campground");
-const Review = require("./models/review");
+import { campgroundSchema, reviewSchema } from "./schemas.js";
+import ExpressError from "./utils/ExpressError";
+import { findById } from "./models/campground";
+import { findById as _findById } from "./models/review";
 
-module.exports.isLoggedIn = (req, res, next) => {
+export function isLoggedIn(req, res, next) {
   if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
     req.flash("error", "Must be signed in");
     return res.redirect("/login");
   }
   next();
-};
+}
 
-module.exports.validateCampground = (req, res, next) => {
+export function validateCampground(req, res, next) {
   const { error } = campgroundSchema.validate(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(",");
@@ -20,29 +20,29 @@ module.exports.validateCampground = (req, res, next) => {
   } else {
     next();
   }
-};
+}
 
-module.exports.isAuthor = async (req, res, next) => {
+export async function isAuthor(req, res, next) {
   const { id } = req.params;
-  const campground = await Campground.findById(id);
+  const campground = await findById(id);
   if (!campground.author.equals(req.user._id)) {
     req.flash("error", "Permission Denied");
     return res.redirect(`/campgrounds/${id}`);
   }
   next();
-};
+}
 
-module.exports.isReviewAuthor = async (req, res, next) => {
+export async function isReviewAuthor(req, res, next) {
   const { id, reviewId } = req.params;
-  const review = await Review.findById(reviewId);
+  const review = await _findById(reviewId);
   if (!review.author.equals(req.user._id)) {
     req.flash("error", "Permission Denied");
     return res.redirect(`/campgrounds/${id}`);
   }
   next();
-};
+}
 
-module.exports.validateReview = (req, res, next) => {
+export function validateReview(req, res, next) {
   const { error } = reviewSchema.validate(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(",");
@@ -50,4 +50,4 @@ module.exports.validateReview = (req, res, next) => {
   } else {
     next();
   }
-};
+}
